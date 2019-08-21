f2bfa18444af4c2a80abc6b168ecf51c9d2540dd
Natatisha <natatisha94@gmail.com>
2019-08-20 17:14:17 -0700

    final model

--- model.py
+++ model.py
@@ -1,66 +1,72 @@
 import numpy as np
 import pandas as pd
 import matplotlib.image as mpimg
-import cv2
+import matplotlib.pyplot as plt
 import os
 
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 from keras.optimizers import Adam
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Conv2D, MaxPool2D, Lambda, Cropping2D
-from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras import regularizers
 
 IMG_SHAPE = (160, 320, 3)
 
 
-def load_data(folder_path, file_name):
-    # load data
+def load_samples(folder_path, file_name):
     data = pd.read_csv(folder_path + file_name)
-
-    # get center, left and right images
     images = pd.concat([data['center'], data['left'], data['right']])
 
-    n = len(images)
-
-    # allocate memory for loaded and flipped images (n * 2)
-    X_train = np.empty(shape=((n, *IMG_SHAPE)), dtype=np.float32)
-
-    # augment images and put all images into pre-allocated array
-    for i, path in enumerate(images):
-        image = mpimg.imread(folder_path + "IMG/" + os.path.basename(path))
-        X_train[i] = image
-    #         X_train[n + i] = cv2.flip(image, 1)
-
-    # extract steering data
-    steering_center = np.array(data['steering'], dtype=np.float32)
-
-    # generate corrected steering data for left and right images
-    correction = 0.23
+    steering_center = data['steering']
+    correction = 0.24
     steering_left = steering_center + correction
     steering_right = steering_center - correction
+    measurements = pd.concat([steering_center, steering_left, steering_right])
 
-    # put all measurements together into numpy array
-    y_train = np.concatenate(
-        (steering_center, steering_left, steering_right))
+    print("Loaded {} samples".format(len(images)))
+    return pd.concat([images, measurements], axis=1)
 
-    return X_train, y_train
+
+def generator(samples, batch_size=32, folder_path="./data/"):
+    num_samples = len(samples)
+    while True:
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples.iloc[offset:offset + batch_size]
+
+            images = []
+            angles = []
+            for i, batch_sample in batch_samples.iterrows():
+                name = (folder_path + "IMG/" + os.path.basename(batch_sample[0]))
+                image = mpimg.imread(name)
+                angle = float(batch_sample['steering'])
+                images.append(image)
+                angles.append(angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images, dtype=np.float32)
+            y_train = np.array(angles, dtype=np.float32)
+            yield shuffle(X_train, y_train)
 
 
 def build_network():
+    beta = 0.01
     model = Sequential()
     model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=IMG_SHAPE))
     model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 
-    model.add(Conv2D(kernel_size=5, filters=24, activation="relu"))
-    model.add(Conv2D(kernel_size=5, filters=36, activation="relu"))
+    model.add(Conv2D(kernel_size=5, filters=24, activation="relu", kernel_regularizer=regularizers.l2(beta)))
+    model.add(Conv2D(kernel_size=5, filters=36, activation="relu", kernel_regularizer=regularizers.l2(beta)))
     model.add(MaxPool2D())
 
-    model.add(Conv2D(kernel_size=5, filters=48, activation="relu"))
-    model.add(Conv2D(kernel_size=3, filters=64, activation="relu"))
+    model.add(Conv2D(kernel_size=5, filters=48, activation="relu", kernel_regularizer=regularizers.l2(beta)))
+    model.add(Conv2D(kernel_size=3, filters=64, activation="relu", kernel_regularizer=regularizers.l2(beta)))
     model.add(MaxPool2D())
 
-    model.add(Conv2D(kernel_size=3, filters=64, activation="relu"))
+    model.add(Conv2D(kernel_size=3, filters=64, activation="relu", kernel_regularizer=regularizers.l2(beta)))
     model.add(Flatten())
     model.add(Dense(100))
     model.add(Dense(50))
@@ -70,31 +76,39 @@ def build_network():
     return model
 
 
+def plot_learning_curve():
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
 if __name__ == '__main__':
-    epochs = 5
+    epochs = 8
     batch_size = 32
 
-    X_train, y_train = load_data("./data/", "driving_log.csv")
-    print(X_train.shape)
-    print(y_train.shape)
+    samples = load_samples("./data/", "driving_log.csv")
+    train_samples, validation_samples = train_test_split(samples, test_size=0.2, shuffle=True)
+    print(train_samples.shape)
+    print(validation_samples.shape)
 
-    # X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.2, shuffle=True)
+    train_generator = generator(train_samples, batch_size=batch_size)
+    validation_generator = generator(validation_samples, batch_size=batch_size)
 
     model = build_network()
     model.summary()
 
-    # steps_train = len(X_train) // batch_size
-    # steps_valid = len(X_valid) // batch_size
-    # train_datagen = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True,
-    #                                    horizontal_flip=True)
-    #
-    # train_datagen.fit(X_train)
-    # train_datagen.standardize(X_valid)
-    #
-    # training_generator = train_datagen.flow(X_train, y_train, batch_size=batch_size)
+    steps_train = len(train_samples) // batch_size
+    steps_valid = len(validation_samples) // batch_size
 
-    # model.fit_generator(training_generator, steps_per_epoch=steps_train,
-    #                     validation_data=(X_valid, y_valid), validation_steps=steps_valid,
-    #                     epochs=epochs)
-    model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2, shuffle=True)
-    model.save('model.h5')
+    checkpoint = ModelCheckpoint('model.h5', monitor='val_loss', verbose=1, save_best_only=True)
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=2)
+
+    history = model.fit_generator(train_generator, steps_per_epoch=steps_train,
+                                  validation_data=validation_generator, validation_steps=steps_valid,
+                                  epochs=epochs, callbacks=[checkpoint, es])
+
+    plot_learning_curve()
